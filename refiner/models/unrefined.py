0a54faf88c0e1194b04e5be02e0ab0b8e7c29153
Nicholas A Hrboka <nicholashrboka@gmail.com>
2025-05-27 00:00:46 +0200

    fixes

--- refiner/models/unrefined.py
+++ refiner/models/unrefined.py
@@ -1,5 +1,5 @@
-from typing import Optional
-from pydantic import BaseModel
+from typing import Optional, Union
+from pydantic import BaseModel, Field
 from datetime import datetime
 
 class CheckIn(BaseModel):
@@ -9,9 +9,21 @@ class CheckIn(BaseModel):
     health_comment: Optional[str] = None
     doctor_visit: Optional[bool] = None
     health_profile_update: Optional[bool] = None
-    anxiety_level: Optional[str] = None
+    anxiety_level: Optional[Union[str, int]] = None
     anxiety_details: Optional[str] = None
-    pain_level: Optional[int] = None
+    pain_level: Optional[Union[str, int]] = None
     pain_details: Optional[str] = None
-    fatigue_level: Optional[int] = None
+    fatigue_level: Optional[Union[str, int]] = None
     fatigue_details: Optional[str] = None
+
+    class Config:
+        from_attributes = True
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        if isinstance(self.anxiety_level, int):
+            self.anxiety_level = str(self.anxiety_level)
+        if isinstance(self.pain_level, int):
+            self.pain_level = str(self.pain_level)
+        if isinstance(self.fatigue_level, int):
+            self.fatigue_level = str(self.fatigue_level)
